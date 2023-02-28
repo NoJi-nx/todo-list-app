ea2b79a45fcc378d21919f28a5f32464af2490fb
Nejirwan <37329734+NRiCe31@users.noreply.github.com>
2023-02-28 04:10:34 +0100

    Ansluten till databas i index

--- src/index.php
+++ src/index.php
@@ -1,23 +1,7 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TODO1</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-</head>
-<body>
-    <div class="container">
-        <div class="row">
-            <h3>TODO</h3>
-            <div>
-                <input type="text" name=" " class="form-control">
-            </div>
-            <div>
-            <button>ADD</button>
-            </div>
-        </div>
-    </div>
-    
-</body>
-</html>
+<!-- -->
+<?php
+session_start();
+if (!isset($_SESSION['username'])) {
+  header('location: login.php');
+}
+include 'dbConnection.php';
