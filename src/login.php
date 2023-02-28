6a217bcda23ef94641cb8d3509bc4c3bd502b06c
Nejirwan <37329734+NRiCe31@users.noreply.github.com>
2023-02-28 05:40:30 +0100

    lagt till formulär för ingloggningen

--- src/login.php
+++ src/login.php
@@ -19,3 +19,22 @@ if (isset($_POST['username'])) {
   }
 }
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Login</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <div class="container">
+    <form action="" method="POST">
+      <h1>Login</h1>
+      <input type="text" name="username" placeholder="Username" required>
+      <input type="password" name="password" placeholder="Password" required>
+      <button type="submit">Login</button>
+    </form>
+    <p>Don't have an account? <a href="register.php">Register</a></p>
+  </div>
+</body>
+</html>
