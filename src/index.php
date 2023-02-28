503ae5545d3b7e2aba10a8a20701ad1ed9b8f105
Nejirwan <37329734+NRiCe31@users.noreply.github.com>
2023-02-28 04:49:25 +0100

    Skapade CRUD metoden i index-filen

--- src/index.php
+++ src/index.php
@@ -1,7 +1,52 @@
-<!-- -->
+<!--Referens till dbConnection fil -->
 <?php
 session_start();
 if (!isset($_SESSION['username'])) {
   header('location: login.php');
 }
 include 'dbConnection.php';
+
+//CRUD Metoden
+
+//Visar uppgifter
+if (isset($_POST['title'])) {
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $user_id = $_SESSION['user_id'];
+    $query = "INSERT INTO tasks (title, description, user_id) VALUES ('$title', '$description', '$user_id')";
+    mysqli_query($conn, $query);
+  }
+  
+  //Ändra uppgifter
+  if (isset($_POST['edit'])) {
+    $task_id = $_POST['task_id'];
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $query = "UPDATE tasks SET title='$title', description='$description' WHERE id='$user_id'";
+    mysqli_query($conn, $query);
+  }
+
+  //Ta bort
+if (isset($_POST['delete'])) {
+    $user_id = $_POST['user_id'];
+    $query = "DELETE FROM tasks WHERE id='$task_id'";
+    mysqli_query($conn, $query);
+  }
+  
+  //Markera
+  if (isset($_POST['mark_complete'])) {
+    $user_id= $_POST['user_id'];
+    $query = "UPDATE tasks SET completed=1 WHERE id='$task_id'";
+    mysqli_query($conn, $query);
+  }
+  
+  //Ta bort markerade
+  if (isset($_POST['delete_completed'])) {
+    $user_id = $_SESSION['user_id'];
+    $query = "DELETE FROM tasks WHERE user_id='$user_id' AND completed=1";
+    mysqli_query($conn, $query);
+  }
+  $user_id = $_SESSION['user_id'];
+  $query = "SELECT * FROM tasks WHERE user_id='$user_id'";
+  $result = mysqli_query($conn, $query);
+  ?>
