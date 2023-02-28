40f1d00b341ea7ed725580794ff6b3adec7a65b0
Nejirwan <37329734+NRiCe31@users.noreply.github.com>
2023-02-28 08:53:02 +0100

    modellerat index filen

--- src/dbconnection.php
+++ src/dbconnection.php
@@ -1,8 +1,11 @@
-<!--Anslutning till databasen-->
 <?php
+
+//Anslutning till databasen
+
 $conn = mysqli_connect('db', 'root', 'example', 'todo_list');
 
 if (!$conn) {
   die('Connection failed: ' . mysqli_connect_error());
 }
 ?>
+
--- src/index.php
+++ src/index.php
@@ -1,101 +1,155 @@
-<!--Referens till dbConnection fil -->
 <?php
 session_start();
 if (!isset($_SESSION['username'])) {
   header('location: login.php');
 }
-include 'dbConnection.php';
+include './dbConnection.php';
 
-//CRUD Metoden och anslutning till databas tabellerna
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-//Visar uppgifter
-if (isset($_POST['title'])) {
+  // Add task
+  if (isset($_POST['add_task'])) {
     $title = $_POST['title'];
     $description = $_POST['description'];
     $user_id = $_SESSION['user_id'];
-    $query = "INSERT INTO tasks (title, description, user_id) VALUES ('$title', '$description', '$user_id')";
-    mysqli_query($conn, $query);
-  }
-  
-  //Ändra uppgifter
-  if (isset($_POST['edit'])) {
-    $task_id = $_POST['task_id'];
-    $title = $_POST['title'];
-    $description = $_POST['description'];
-    $query = "UPDATE tasks SET title='$title', description='$description' WHERE id='$user_id'";
-    mysqli_query($conn, $query);
+    $query = "INSERT INTO tasks (title, description, user_id) VALUES (?, ?, ?)";
+    $stmt = mysqli_prepare($conn, $query);
+    mysqli_stmt_bind_param($stmt, "ssi", $title, $description, $user_id);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
   }
 
-  //Ta bort
-if (isset($_POST['delete'])) {
-    $user_id = $_POST['user_id'];
-    $query = "DELETE FROM tasks WHERE id='$task_id'";
-    mysqli_query($conn, $query);
+  // Edit task
+  if (isset($_POST['edit_task'])) {
+    $id = $_POST['id'];
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $query = "UPDATE tasks SET title=?, description=? WHERE id=?";
+    $stmt = mysqli_prepare($conn, $query);
+    mysqli_stmt_bind_param($stmt, "ssi", $title, $description, $id);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
   }
-  
-  //Markera
+
+  // Delete task
+  if (isset($_POST['delete_task'])) {
+    $id = $_POST['id'];
+    $query = "DELETE FROM tasks WHERE id=?";
+    $stmt = mysqli_prepare($conn, $query);
+    mysqli_stmt_bind_param($stmt, "i", $id);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+  }
+
+  // Mark task as completed
   if (isset($_POST['mark_complete'])) {
-    $user_id= $_POST['user_id'];
-    $query = "UPDATE tasks SET completed=1 WHERE id='$task_id'";
-    mysqli_query($conn, $query);
+    $id = $_POST['id'];
+    $query = "UPDATE tasks SET completed=1 WHERE id=?";
+    $stmt = mysqli_prepare($conn, $query);
+    mysqli_stmt_bind_param($stmt, "i", $id);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
   }
-  
-  //Ta bort markerade
+
+  // Mark task as incomplete
+  if (isset($_POST['mark_incomplete'])) {
+    $id = $_POST['id'];
+    $query = "UPDATE tasks SET completed=0 WHERE id=?";
+    $stmt = mysqli_prepare($conn, $query);
+    mysqli_stmt_bind_param($stmt, "i", $id);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+  }
+
+  // Delete completed tasks
   if (isset($_POST['delete_completed'])) {
     $user_id = $_SESSION['user_id'];
-    $query = "DELETE FROM tasks WHERE user_id='$user_id' AND completed=1";
-    mysqli_query($conn, $query);
+    $query = "DELETE FROM tasks WHERE user_id=? AND completed=1";
+    $stmt = mysqli_prepare($conn, $query);
+    mysqli_stmt_bind_param($stmt, "i", $user_id);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
   }
-  $user_id = $_SESSION['user_id'];
-  $query = "SELECT * FROM tasks WHERE user_id='$user_id'";
-  $result = mysqli_query($conn, $query);
-  ?>
+}
+
+$user_id = $_SESSION['user_id'];
+$query = "SELECT * FROM tasks WHERE user_id=?";
+$stmt = mysqli_prepare($conn, $query);
+mysqli_stmt_bind_param($stmt, "i", $user_id);
+mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
+$tasks = mysqli_fetch_all($result, MYSQLI_ASSOC);
+mysqli_stmt_close($stmt);
+mysqli_close($conn);
+?>
 
 <!DOCTYPE html>
-  <html lang="en">
-  <head>
-    <title>Todo List</title>
-    <link rel="stylesheet" type="text/css" href="./styles/style.css">
-  </head>
-  <body>
-  <div class="header">
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Task List</title>
+  <link rel="stylesheet" type="text/css" href="./styles/style.css">
+</head>
+<body>
+<div class="header">
     <h1>Todo List</h1>
     <form action="logout.php">
       <button>Logout</button> <!--Logga utt knappen -->
     </form>
   </div>
-
-
-  <!--Formulär -->
-  <div class="container">
-    <form action="" method="POST">
+  <div>
+    <form method="POST">
       <input type="text" name="title" placeholder="Title" required>
       <textarea name="description" placeholder="Description" required></textarea>
-      <button type="submit">Add Task</button> <!--Lägga till uppgift-->
-    </form>
-    <hr>
-    <?php while ($row = mysqli_fetch_assoc($result)) { ?>
-      <div class="task">
-        <form action="" method="POST">
-        <form action="" method="POST">
-  <input type="hidden" name="user_id" value="<?php echo $row['id']; ?>">
-  <input type="text" name="title" value="<?php echo $row['title']; ?>">
-  <textarea name="description"><?php echo $row['description']; ?></textarea>
-  <?php if ($row['completed']) { ?>
-    <input type="checkbox" name="completed" checked disabled>
-  <?php } else { ?>
-    <button type="submit" name="mark_complete">Mark Complete</button>
-  <?php } ?>
-  <button type="submit" name="edit">Save</button> <!--Ändra uppgift-->
-  <button type="submit" name="delete">Delete</button> <!--Ta bort-->
-</form>
-      </div>
-    <?php } ?>
-    <hr>
-    <form action="" method="POST">
-      <button type="submit" name="delete_completed">Delete Completed Tasks</button>
+      <button type="submit" name="add_task">Add Task</button>
     </form>
   </div>
+  <table>
+    <thead>
+      <tr>
+        <th>Title</th>
+        <th>Description</th>
+        <th>Status</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach ($tasks as $task) { ?>
+      <tr>
+        <td><?php echo $task['title']; ?></td>
+        <td><?php echo $task['description']; ?></td>
+        <td>
+          <?php if ($task['completed']) { ?>
+            <span class="completed">Completed</span>
+          <?php } else { ?>
+            <span class="incomplete">Incomplete</span>
+          <?php } ?>
+        </td>
+        <td>
+          <?php if (!$task['completed']) { ?>
+            <form method="POST">
+              <input type="hidden" name="id" value="<?php echo $task['id']; ?>">
+              <button type="submit" name="mark_complete">Mark as Completed</button>
+            </form>
+          <?php } ?>
+          <form method="POST">
+            <input type="hidden" name="id" value="<?php echo $task['id']; ?>">
+            <input type="text" name="title" value="<?php echo $task['title']; ?>" required>
+            <textarea name="description" required><?php echo $task['description']; ?></textarea>
+            <button type="submit" name="edit_task">Update</button>
+            <button type="submit" name="delete_task">Delete</button>
+          </form>
+        </td>
+      </tr>
+      <?php } ?>
+    </tbody>
+  </table>
+  <form method="POST">
+    <button type="submit" name="delete_completed">Delete Completed Tasks</button>
+  </form>
 </body>
 </html>
+
+
+
     
